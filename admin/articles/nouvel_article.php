b2a0f6a65bf170a6067f1743c98982d253d978c9
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-03-06 16:22:33 +0100

    newArticle app step 1

--- admin/articles/nouvel_article.php
+++ admin/articles/nouvel_article.php
@@ -55,20 +55,20 @@ if(!$session->exists("user")){
 <?php include_once "../common/side-bar.php"; ?>
 <?php include_once "../common/top-bar.php"; ?>
 
-<div id="page">
+<div id="page" ng-app="newArticle">
     <div class="container">
         <h1>Nouvel Article</h1>
     </div>
     <div class="container">
         <div class="row">
-            <div class="col col-9">
+            <div class="col col-9" ng-controller="NewAricleFormCtrl">
                 <form class="form" action="" method="post">
 
                     <label for="designation">Désignation/nom de l'article*</label>
-                    <input type="text" name="new_item_form[designation]" id="designation"/>
+                    <input type="text" name="new_item_form[designation]" id="designation" ng-blur="designationBlur()" ng-model="item.name"/>
 
                     <label for="reference">Référence*</label>
-                    <input type="text" name="new_item_form[reference]" id="reference"/>
+                    <input type="text" name="new_item_form[reference]" id="reference" ng-blur="referenceBlur()" ng-model="item.reference"/>
 
                     <label for="desription">Description</label>
                     <textarea name="new_item_form[description]" id="desription"></textarea>
@@ -98,7 +98,7 @@ if(!$session->exists("user")){
 
                     <!-- TODO[Nicolas] media associé -->
 
-                    <button class="btn simple-blue">Enregister</button>
+                    <button class="btn simple-blue" ng-click="submit()">Enregister</button>
 
                     <p>(*) Champs obligatoires</p>
                 </form>
@@ -111,9 +111,11 @@ if(!$session->exists("user")){
 </div>
 
 <script src="/admin/js/vendor/jquery-1.11.0.min.js"></script>
+<script src="/admin/js/vendor/angular.min.js"></script>
 <script src="/admin/js/jquery-plugins/menu-accordeon.js"></script>
 <script>
     $(".menu-accordeon").menuAccordeon();
 </script>
+<script src="/admin/js/apps/new-article/main.js"></script>
 </body>
 </html>
--- admin/js/apps/new-article/main.js
+++ admin/js/apps/new-article/main.js
@@ -2,4 +2,32 @@
  * Created by Nicolas on 06/03/14.
  */
 
-var app = angular.module("newArticle", []);
+var app = angular.module("newArticle", [])
+    .config(["$httpProvider", function($httpProvider){
+        $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+    }]);
+
+
+
+app.controller("NewAricleFormCtrl", ["$scope", "$http", function($scope, $http){
+
+    $scope.item = {
+        "name": "",
+        "reference": ""
+    };
+
+    $scope.designationBlur = function () {
+        //console.log("designation:" + $scope.item.name);
+        // test de validité
+    };
+
+    $scope.referenceBlur = function () {
+        console.log("reference:" + $scope.item.reference);
+        // test de validité
+    };
+
+    $scope.submit = function () {
+
+    };
+
+}]);
